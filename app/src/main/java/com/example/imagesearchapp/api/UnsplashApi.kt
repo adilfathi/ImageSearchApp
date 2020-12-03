b8e117f080cc909d11899664b1d6676bc29bfb3b
adilfathiabdillah <adlftha3625@gmail.com>
2020-12-03 09:20:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/imagesearchapp/api/UnsplashApi.kt
@@ -0,0 +1,22 @@
+package com.example.imagesearchapp.api
+
+import androidx.viewbinding.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+        const val CLIENT_ID = "2B_f3x5FqbeeaN-o_G_50h6Z5y4xvoDrxFnLBVEx9LI"
+    }
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): UnsplashResponse
+}
